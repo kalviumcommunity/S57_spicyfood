8626ab2a39d40a85336f79050e1b8e94231933e5
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2024-07-06 12:29:40 +0530

    done

---
+++ backend/schema.js
@@ -0,0 +1,26 @@
+const mongoose= require('mongoose')
+
+const data = new mongoose.Schema({
+
+    Dish_Name:{
+        type:String,
+        required:true
+    },
+    type:{
+        type:String,
+        required:true
+
+    },
+    Ingridents:{
+        type:String,
+        require:true
+
+    },
+    Origin:{
+        type:String,
+        require:true
+    },
+
+});
+const dataset = mongoose.model('dataset',data);
+module.exports= dataset
