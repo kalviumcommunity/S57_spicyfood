6b196a0feb51b0f4bb057dbc9da6ef5790391bdc
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2024-07-07 21:31:54 +0530

    backend started

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const app=express();
 const mongoose=require('mongoose');
 const {connectDb,checkConnected }=require('./db');
 const port= 3000;
-// app.get('/ping',(req,res)=>{
-//     res.send("pong")
+app.get('/ping',(req,res)=>{
+    res.send("pong")
 
-// })
+})
 app.use(express.json());
 
 connectDb()
