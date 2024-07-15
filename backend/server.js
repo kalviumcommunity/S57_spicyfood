c4e7581af87f31929c270206cd2501f37fc7c9f5
harini18122005 <144237501+harini18122005@users.noreply.github.com>
2024-07-15 11:34:25 +0530

    push2

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,25 @@ const app=express();
 const mongoose=require('mongoose');
 const {connectDb,checkConnected }=require('./db');
 const port= 3000;
-app.get('/ping',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send("pong")
 
 })
 app.use(express.json());
 
-connectDb()
-app.listen(port,()=>{
-    console.log("port is running successful")
+app.listen(port,async()=>{
+    try{
+       await connectDb()
+       if(checkConnected()){
+        
+        console.log("port is running successful")
+       }
+       
+    }
+    catch(error){
+        console.log(error)
+    }
+    
 })
 
 
